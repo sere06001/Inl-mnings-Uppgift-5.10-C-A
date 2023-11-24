95caa5d14b87b1cb0366abda81c4c3b343747ccc
sere06001 <sere06001@utb.vaxjo.se>
2023-11-24 22:57:54 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.10-C-A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Uppgift5_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = { 1, 2, 3, 4, 4, 9, 9, 5, 9};
+            int maxantal = 0;
+            int mestFrekventTal = 0;
+            for (int i = 0; i < tal.Length; i++)
+            {
+                int antal = 0;
+                for (int j = 0; j < tal.Length; j++)
+                {
+                    if (tal[i] == tal[j])
+                    {
+                        antal++;
+                    }
+                }
+
+                if (antal > maxantal)
+                {
+                    maxantal = antal;
+                    mestFrekventTal = tal[i];
+                }
+            }
+            Console.WriteLine("Följande tal är vanligast: "+mestFrekventTal);
+            Console.ReadKey();
+        }
+    }
+}
